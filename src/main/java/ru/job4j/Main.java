9f6491c58a78d858d7a6f722be0f263503e7a7e5
KATAR5I5 <malex999@bk.ru>
2023-02-11 14:47:50 +0300

    3.1. Создание репозитория и привязка к исходному коду. [#504809 #390542]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("выарлывыаыавф world");
+        System.out.println("Русский поддерживается");
     }
 }
